44f6e424174d417170b1869d2f52599bdb9c7fa4
Daniel Seemaier <daniel@seemaier.de>
2020-04-14 22:33:25 +0200

    implement topord initial partitioner

--- kahypar/partition/dhgp/initial_partitioning/acyclic_topord_initial_partitioner.h
+++ kahypar/partition/dhgp/initial_partitioning/acyclic_topord_initial_partitioner.h
@@ -5,6 +5,7 @@
 #include "kahypar/definitions.h"
 #include "kahypar/partition/initial_partitioning/i_initial_partitioner.h"
 #include "kahypar/partition/initial_partitioning/initial_partitioner_base.h"
+#include "kahypar/partition/dhgp/topord.h"
 
 namespace kahypar::dhgp {
 class AcyclicTopOrdInitialPartitioner : public IInitialPartitioner,
@@ -25,11 +26,27 @@ class AcyclicTopOrdInitialPartitioner : public IInitialPartitioner,
   AcyclicTopOrdInitialPartitioner& operator= (AcyclicTopOrdInitialPartitioner&&) = delete;
 
  private:
-  void partitionImpl() override final {
+  void partitionImpl() final {
     Base::multipleRunsInitialPartitioning();
   }
 
   void initialPartition() {
+    const PartitionID unassigned_part = _context.initial_partitioning.unassigned_part;
+    _context.initial_partitioning.unassigned_part = -1;
+    Base::resetPartitioning();
+    _context.initial_partitioning.unassigned_part = unassigned_part;
+
+    const auto topord = calculateTopologicalOrdering(_hg, true);
+    PartitionID p = 0;
+    for (const HypernodeID& hn : topord) {
+      if (_hg.partWeight(p) > _context.initial_partitioning.perfect_balance_partition_weight[p]) {
+        ++p;
+      }
+      ASSERT(p < _context.partition.k);
+      _hg.setNodePart(hn, p);
+    }
+
+    _hg.initializeNumCutHyperedges();
   }
 
   using Base::_hg;
